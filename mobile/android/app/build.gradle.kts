14c69c7d4e27d632cd1f4e8b46196020d696d1e2
勒布朗-詹姆斯 <2986253039@qq.com>
2026-09-18 05:38:56 +0000

    fix(android): mobile/android/app/build.gradle.kts (#342 review)

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -32,4 +32,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.json:json:20240303")
+    testImplementation("junit:junit:4.13.2")
 }
